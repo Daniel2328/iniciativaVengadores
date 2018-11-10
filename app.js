a7dd2d778bc65028914ed7bb7d2461b75acbe422
daniel2328 <danielleond@gmail.com>
2018-11-09 21:29:27 -0600

    [0.2] rutas con contenido creado, falta perfeccionar

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var facturacionRouter = require('./routes/facturacion');
+var inventarioRouter = require('./routes/inventario');
+var cuentaCorrienteRouter = require('./routes/cuentaCorriente');
+var ingresosRouter = require('./routes/ingresos');
+var proveedoresRouter = require('./routes/Proveedores');
+var clientesMoraRouter = require('./routes/clientesMora');
+var saldosRouter = require('./routes/saldos');
+var cargosRouter = require('./routes/cargos');
+var abonosRouter = require('./routes/abonos');
+var pagarRouter = require('./routes/pagar');
+var facturasRouter = require('./routes/facturas');
 
 var app = express();
 
@@ -21,6 +32,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/facturacion', facturacionRouter);
+app.use('/inventario', inventarioRouter);
+app.use('/cuentaCorriente', cuentaCorrienteRouter);
+app.use('/ingresos', ingresosRouter);
+app.use('/Proveedores', proveedoresRouter);
+app.use('/clientesMora', clientesMoraRouter);
+app.use('/saldos', saldosRouter);
+app.use('/cargos', cargosRouter);
+app.use('/abonos', abonosRouter);
+app.use('/pagar', pagarRouter);
+app.use('/facturas', facturasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/facturas.js
+++ routes/facturas.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pacific Oil S. A.' });
+  res.render('views/impresion/facturas', {
+    title: 'Pacific Oil S. A.'
+  });
 });
 
 module.exports = router;
